750ff858f42fe3e6b42216433612c90184cf486d
quaja-abu <acabuchane@gmail.com>
2021-07-28 11:46:26 +0300

    update header file

--- holberton.h
+++ holberton.h
@@ -6,23 +6,4 @@
 
 int _printf(const char *format, ...);
 
-int _strlen(const char *)
-{
-	int j = 0;
-	int count = 0;
-
-	while (*(s + j) != '\0')
-	{
-		count = count + 1;
-		i++;
-	}
-	return (count);
-}
-
-int _putchar(const char *)
-{
-		int c;
-	        return (write(1, &c, 1));
-}
-
 #endif /* HOLBERTON_H */

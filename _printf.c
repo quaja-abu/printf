8144de3a6c3fdf738a9eab2879d2a90ce14d5af1
quaja-abu <acabuchane@gmail.com>
2021-07-28 11:46:04 +0300

    debug code and fix function definition

--- _printf.c
+++ _printf.c
@@ -1,4 +1,7 @@
 #include "holberton.h"
+int _strlen(const char *);
+void _putchar(const char *);
+
 /**
   * _printf - function that prints anything
   * @format: charater pointer that takes strings as argument
@@ -9,13 +12,11 @@ int _printf(const char *format, ...)
 {
 	const char *str, *s;
 	va_list args;
-	int len; 
-	int val;
-	int i;
+	int val, len = 0;
 
 	len = _strlen(format);
 	va_start(args, format);
-	for (str = 0; str != '\0'; str++)
+	for (str = format; *str != '\0'; str++)
 	{
 		while (*str != '%')
 		{
@@ -23,7 +24,6 @@ int _printf(const char *format, ...)
 			if (*str == '\0')
 				break;
 			str++;
-			i++;
 		}
 		if (*str == '\0')
 			break;
@@ -36,12 +36,9 @@ int _printf(const char *format, ...)
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				while (*s)
-				{
-					putchar(*s);
-					s++;
-				}
-			/*	puts(s);*/
+				len += _strlen(s);
+				len = len - 2;
+				_putchar(s);
 				break;
 			case '%':
 				putchar(*str);
@@ -53,5 +50,38 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(args);
-	return (i);
+	return (len);
+}
+
+/**
+  * _strlen - function to find string length
+  * @s: character pointer
+  *
+  * Return: integer value
+  */
+
+int _strlen(const char *s)
+{
+	int val = 0;
+
+	while (s[val] != '\0')
+	{
+		val++;
+	}
+	return (val);
+}
+/**
+  * _putchar - function to display string
+  * @s: character pointer
+  *
+  * Return: Nothing
+  */
+
+void _putchar(const char *s)
+{
+	while (*s)
+	{
+		putchar(*s);
+		s++;
+	}
 }
